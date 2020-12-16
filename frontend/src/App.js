d7a23ec8048a23ae769faca53df16b589254168a
Yogesh <you@example.com>
2020-12-16 22:33:21 +0530

    React Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>I will Be the Best FullStack Developer</h1>
+    </div>
+  );
+}
+
+export default App;

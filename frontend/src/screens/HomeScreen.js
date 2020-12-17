5da5c5984b49b408c38feed8f87fc896feab2122
Yogesh <you@example.com>
2020-12-17 22:31:15 +0530

    done fetching data from database

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -8,7 +8,7 @@ const HomeScreen = () => {
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
+      const { data } = await axios.get('/api/products'); // destructure data directly instead of res.data
 
       setProducts(data);
     };

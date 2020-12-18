3baae84673b91c24a5e53c1633bba79909c13755
Yogesh <you@example.com>
2020-12-18 17:53:41 +0530

    done add to cart functionality

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -3,9 +3,9 @@ import { CART_ADD_ITEM } from '../constants/cartConstants'
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
-      const item = action.payload
+      const item = action.payload //this is the newitem in to  the cart ...
 
-      const existItem = state.cartItems.find((x) => x.product === item.product)
+      const existItem = state.cartItems.find((x) => x.product === item.product) // returns only found items where condition is true
 
       if (existItem) {
         return {

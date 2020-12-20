e2adfa4b5e1d505fa2a0f4b2ff2d42b50fc54c00
Yogesh-10 <yogiyogesh100800@gmail.com>
2020-12-21 04:39:41 +0530

    shipping screen and progress bar created

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import { Alert } from 'react-bootstrap';
+import React from 'react'
+import { Alert } from 'react-bootstrap'
 
+// children is the prop passed in to Message component from other component
 const Message = ({ variant, children }) => {
-  return <Alert variant={variant}>{children}</Alert>;
-};
+  return <Alert variant={variant}>{children}</Alert>
+}
 
 Message.defaultProps = {
   variant: 'info',
-};
+}
 
 // ****** the above code  also can be writen as error prop that is passed from home screen to message which display error from home screen ****//
 // const Message = ({ variant, error }) => {
 //   return <Alert variant={variant}>{error}</Alert>;
 // };
 
-export default Message;
+export default Message

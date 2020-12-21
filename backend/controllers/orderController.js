f79c509f4e6dd64ee852d72881edfef24e44ff08
Yogesh-10 <yogiyogesh100800@gmail.com>
2020-12-21 22:43:35 +0530

    Get order by id created in backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -34,4 +34,20 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 })
 
-export { addOrderItems }
+// @desc Get order bt id
+// @route GET/api/orders/:id
+// @access  private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  )
+  if (order) {
+    res.status(200)
+    res.json(order)
+  } else {
+    throw new Error('Order Not Found')
+  }
+})
+
+export { addOrderItems, getOrderById }
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 const router = express.Router()
-import { addOrderItems } from '../controllers/orderController.js'
+import { addOrderItems, getOrderById } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItems)
+router.route('/:id').get(protect, getOrderById) //this should be always below to get the route correctly
 
 export default router

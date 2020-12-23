e5be697416f35f9a9d20b080edc722934affc0d4
Yogesh-10 <yogiyogesh100800@gmail.com>
2020-12-23 20:46:57 +0530

    done with setup of admin login and see users list

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -15,6 +15,9 @@ import {
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
   USER_DETAILS_RESET,
+  USER_LIST_SUCCESS,
+  USER_LIST_FAIL,
+  USER_LIST_REQUEST,
 } from '../constants/userConstants'
 
 export const login = (email, password) => async (dispatch) => {
@@ -165,6 +168,40 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
         : error.message
     dispatch({
       type: USER_UPDATE_PROFILE_FAIL,
+      payload: message,
+    })
+  }
+}
+
+export const listUsers = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_LIST_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.get(`/api/users`, config)
+
+    dispatch({
+      type: USER_LIST_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    dispatch({
+      type: USER_LIST_FAIL,
       payload: message,
     })
   }

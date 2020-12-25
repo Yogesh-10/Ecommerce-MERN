cb3d60fb831d2c402cdc1c848bb74b2ba07d222e
Yogesh-10 <yogiyogesh100800@gmail.com>
2020-12-25 08:23:20 +0530

    product delete by admin successful

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -40,4 +40,68 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 })
 
-export { getProducts, getProductById, deleteProduct }
+// @desc create a product
+// @route POST/api/products
+// @access  private/ admin
+
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'sample brand',
+    category: 'sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'sample desc',
+  })
+
+  const createProduct = await product.save()
+  res.status(201).json(createProduct)
+})
+
+// @desc update a product
+// @route PUT/api/products/:id
+// @access  private/ admin
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body
+
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    product.name = name
+    product.price = price
+    product.description = description
+    product.image = image
+    product.brand = brand
+    product.category = category
+    product.countInStock = countInStock
+
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
+  } else {
+    res.status(404)
+    throw new Error('Product Not Found')
+  }
+
+  const createProduct = await product.save()
+  res.status(201).json(createProduct)
+})
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+}

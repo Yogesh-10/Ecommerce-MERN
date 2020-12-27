f6a62873b1a5371b94f993d3fda7518dcef50792
Yogesh <you@example.com>
2020-12-27 22:16:46 +0530

    admin orders list and mark as delivered option added

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -3,12 +3,18 @@ import {
   ORDER_CREATE_FAIL,
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_SUCCESS,
+  ORDER_DELIVER_FAIL,
+  ORDER_DELIVER_REQUEST,
+  ORDER_DELIVER_SUCCESS,
   ORDER_DETAILS_FAIL,
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
+  ORDER_LIST_FAIL,
   ORDER_LIST_MY_FAIL,
   ORDER_LIST_MY_REQUEST,
   ORDER_LIST_MY_SUCCESS,
+  ORDER_LIST_REQUEST,
+  ORDER_LIST_SUCCESS,
   ORDER_PAY_FAIL,
   ORDER_PAY_REQUEST,
   ORDER_PAY_SUCCESS,
@@ -27,7 +33,7 @@ export const createOrder = (order) => async (dispatch, getState) => {
 
     const config = {
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json', //if any data is sent this should be added
         Authorization: `Bearer ${userInfo.token}`,
       },
     }
@@ -128,6 +134,44 @@ export const payOrder = (orderID, paymentResult) => async (
   }
 }
 
+export const deliverOrder = (order) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_DELIVER_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.put(
+      `/api/orders/${order._id}/deliver`,
+      {},
+      config
+    ) // api/orders/:id.. in which the id is which that is passed in the paramter above
+
+    dispatch({
+      type: ORDER_DELIVER_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    dispatch({
+      type: ORDER_DELIVER_FAIL,
+      payload: message,
+    })
+  }
+}
+
 export const listMyOrders = () => async (dispatch, getState) => {
   // no need to pass anything in paramter bcoz it knows who we are by tokens
   try {
@@ -158,6 +202,41 @@ export const listMyOrders = () => async (dispatch, getState) => {
         : error.message
     dispatch({
       type: ORDER_LIST_MY_FAIL,
+      payload: message,
+    })
+  }
+}
+
+export const listOrders = () => async (dispatch, getState) => {
+  // no need to pass anything in paramter bcoz it knows who we are by tokens
+  try {
+    dispatch({
+      type: ORDER_LIST_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.get(`/api/orders`, config)
+
+    dispatch({
+      type: ORDER_LIST_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    dispatch({
+      type: ORDER_LIST_FAIL,
       payload: message,
     })
   }

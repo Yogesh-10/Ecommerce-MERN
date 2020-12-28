833bf5112f3a4cbe729134681a0c25cefe26861e
Yogesh-10 <yogiyogesh100800@gmail.com>
2020-12-28 20:13:12 +0530

    Review and ratings feature added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
@@ -14,6 +15,11 @@ dotenv.config()
 connectDB()
 
 const app = express()
+
+//HTTP request logger middleware that logs in to console
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use(express.json())
 

8e32636d0b270bcf5c05becfb8694f0bac8f3a57
Yogesh-10 <yogiyogesh100800@gmail.com>
2021-02-11 17:36:04 +0530

    bug fix payment screen

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -6,43 +6,43 @@ import { savePaymentMethod } from '../actions/cartActions'
 import CheckoutSteps from '../components/CheckoutSteps'
 
 const PaymentScreen = ({ history }) => {
-  const dispatch = useDispatch()
+	const dispatch = useDispatch()
 
-  const cart = useSelector((state) => state.cart)
-  const { shippingAddress } = cart
+	const cart = useSelector((state) => state.cart)
+	const { shippingAddress } = cart
 
-  if (!shippingAddress) {
-    history.push('/shipping')
-  }
-  //   the values in the usestate comes from the form filled in (from the parameter of savePaymentMethod.. which is passed in as data)
+	if (!shippingAddress.address) {
+		history.push('/shipping')
+	}
+	//   the values in the usestate comes from the form filled in (from the parameter of savePaymentMethod.. which is passed in as data)
 
-  const [paymentMethod, setPaymentMethod] = useState('PayPal')
+	const [paymentMethod, setPaymentMethod] = useState('PayPal')
 
-  const submitHandler = (e) => {
-    e.preventDefault()
-    dispatch(savePaymentMethod(paymentMethod))
-    history.push('/placeorder')
-  }
+	const submitHandler = (e) => {
+		e.preventDefault()
+		dispatch(savePaymentMethod(paymentMethod))
+		history.push('/placeorder')
+	}
 
-  return (
-    <FormContainer>
-      <CheckoutSteps step2 step1 step3 />
-      <h1>Payment Method</h1>
-      <Form onSubmit={submitHandler}>
-        <Form.Group>
-          <Form.Label as='legend'>Select Method</Form.Label>
-          <Col>
-            <Form.Check
-              type='radio'
-              label='PayPal or Credit Card'
-              id='PayPal'
-              name='paymentMethod'
-              value='PayPal'
-              checked
-              onChange={(e) => setPaymentMethod(e.target.value)}
-            ></Form.Check>
+	return (
+		<FormContainer>
+			<CheckoutSteps step2 step1 step3 />
+			<h1>Payment Method</h1>
+			<Form onSubmit={submitHandler}>
+				<Form.Group>
+					<Form.Label as='legend'>Select Method</Form.Label>
+					<Col>
+						<Form.Check
+							type='radio'
+							label='PayPal or Credit Card'
+							id='PayPal'
+							name='paymentMethod'
+							value='PayPal'
+							checked
+							onChange={(e) => setPaymentMethod(e.target.value)}
+						></Form.Check>
 
-            {/* <Form.Check
+						{/* <Form.Check
               type='radio'
               label='stripe'
               id='stripe'
@@ -50,15 +50,15 @@ const PaymentScreen = ({ history }) => {
               value='stripe'
               onChange={(e) => setPaymentMethod(e.target.value)}
             ></Form.Check> */}
-          </Col>
-        </Form.Group>
+					</Col>
+				</Form.Group>
 
-        <Button type='submit' variant='primary'>
-          Continue
-        </Button>
-      </Form>
-    </FormContainer>
-  )
+				<Button type='submit' variant='primary'>
+					Continue
+				</Button>
+			</Form>
+		</FormContainer>
+	)
 }
 
 export default PaymentScreen

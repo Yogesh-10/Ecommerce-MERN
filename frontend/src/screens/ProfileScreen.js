61a715356ee420c08ce841bdafd8f9d54b4596f9
Yogesh <you@example.com>
2021-02-12 14:06:17 +0530

    Bug fix in profile screen(order list)

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -106,21 +106,22 @@ const ProfileScreen = ({ location, history }) => {
 					</Button>
 				</Form>
 			</Col>
+
 			<Col md={9}>
-				<h2>My orders</h2>
+				<h2>My Orders</h2>
 				{loadingOrders ? (
 					<Loader />
 				) : errorOrders ? (
 					<Message variant='danger'>{errorOrders}</Message>
 				) : (
-					<Table striped bordered hover responsive className='table-sm'>
+					<Table striped responsive className='table-sm'>
 						<thead>
 							<tr>
 								<th>ID</th>
-								<th>DATE</th>
-								<th>TOTAL</th>
-								<th>PAID</th>
-								<th>DELIVERED</th>
+								<th>Date</th>
+								<th>Total</th>
+								<th>Paid</th>
+								<th>Delivered</th>
 								<th></th>
 							</tr>
 						</thead>
@@ -128,8 +129,9 @@ const ProfileScreen = ({ location, history }) => {
 						<tbody>
 							{orders.map((order) => (
 								<tr key={order._id}>
-									<td>{order.createdAt}</td>
-									<td>{order.totalPrice}</td>
+									<td>{order._id}</td>
+									<td>{order.createdAt.substring(0, 10)}</td>
+									<td>₹{order.totalPrice}</td>
 									<td>
 										{order.isPaid ? (
 											order.paidAt.substring(0, 10)
@@ -137,19 +139,9 @@ const ProfileScreen = ({ location, history }) => {
 											<i className='fas fa-times' style={{ color: 'red' }}></i>
 										)}
 									</td>
-
-									<td>
-										{order.isDelivered ? (
-											order.deliveredAt.substring(0, 10)
-										) : (
-											<i className='fas fa-times' style={{ color: 'red' }}></i>
-										)}
-									</td>
 									<td>
 										<LinkContainer to={`/order/${order._id}`}>
-											<Button className='btn-sm' variant='light'>
-												Details
-											</Button>
+											<Button className='btn-sm'>Details</Button>
 										</LinkContainer>
 									</td>
 								</tr>
